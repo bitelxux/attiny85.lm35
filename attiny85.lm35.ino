91c30f75d112f6f1b86457864678d9f2c742ed4a
Carlos Novo <bitelxux@gmail.com>
2019-01-13 15:01:21 +0000

    simple average

--- attiny85.lm35.ino
+++ attiny85.lm35.ino
@@ -7,7 +7,9 @@
 #define RX 2
 #define TX 1
 
+float tempPrev = -100;
 float temp;
+float avTemp;
 float voltage;
 int pin0 = 0;
 int pTemp = 3;
@@ -49,12 +51,21 @@ void loop() {
    voltage = getVoltage(pTemp);
    temp = voltage/10;
 
+   // Average last readings to minimice errors
+   if (tempPrev != -100){
+    avTemp = (temp + tempPrev)/2.0;
+    tempPrev = temp;
+   }
+   else {
+    tempPrev = temp;
+   }
+
    MySerial.print("mVolts: ");
    MySerial.print(voltage);
 
    MySerial.print(" ");
    MySerial.print("Temp: ");
-   MySerial.print(temp);
+   MySerial.print(avTemp);
 
    MySerial.print("\n");
 
